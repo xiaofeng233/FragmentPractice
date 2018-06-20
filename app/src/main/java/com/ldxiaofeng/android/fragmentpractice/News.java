c6686a949b8dbb7a182d43ed0780dafec4002ee3
小枫 <1317847793@qq.com>
2018-06-20 23:49:39 +0800

    1

---
+++ app/src/main/java/com/ldxiaofeng/android/fragmentpractice/News.java
@@ -0,0 +1,28 @@
+package com.ldxiaofeng.android.fragmentpractice;
+
+/**
+ * Created by 13178 on 2018-6-14.
+ */
+
+public class News {
+
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/ldxiaofeng/android/fragmentpractice/NewsTitleFragment.java
@@ -0,0 +1,115 @@
+package com.ldxiaofeng.android.fragmentpractice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by 13178 on 2018-6-14.
+ */
+
+public class NewsTitleFragment extends Fragment {
+
+    private boolean isTwoPace;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.news_title_frag,container,false);
+        RecyclerView newsTitleRecyclerView = (RecyclerView) view.findViewById(R.id.news_title_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        newsTitleRecyclerView.setLayoutManager(layoutManager);
+        NewsAdapter adapter = new NewsAdapter(getNews());
+        newsTitleRecyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    private List<News> getNews() {
+        List<News> newsList = new ArrayList<>();
+        for(int i = 0 ;i<50;i++){
+            News news = new News();
+            news.setTitle("这是第新闻标题"+i);
+            news.setContent(getRandomLengthContent("这是第新闻"+ i +". "));
+            newsList.add(news);
+        }
+        return newsList;
+    }
+
+    private String getRandomLengthContent(String s) {
+        Random random = new Random();
+        int length = random.nextInt(20)+1;
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<length;i++){
+            builder.append(s);
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        if(getActivity().findViewById(R.id.news_content_layout)!=null) {
+            isTwoPace = true;
+        }else{
+                isTwoPace = false;
+        }
+    }
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
+        private List<News> mNewsList;
+        class ViewHolder extends RecyclerView.ViewHolder{
+            TextView newsTitleText;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                newsTitleText= (TextView) itemView.findViewById(R.id.news_title);
+            }
+        }
+
+        public NewsAdapter(List<News> newsList){
+            mNewsList = newsList;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item,parent,false);
+            final ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    News news = mNewsList.get(holder.getAdapterPosition());
+                    if(isTwoPace){
+                        NewsContentFragment newsContentFragment = (NewsContentFragment)
+                                getFragmentManager().findFragmentById(R.id.news_content_fragment);
+                        newsContentFragment.refresh(news.getTitle(),news.getContent());
+                    }else{
+                        NewsContentActivity.actionStart(getActivity(),news.getTitle(),news.getContent());
+                    }
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            News news = mNewsList.get(position);
+            holder.newsTitleText.setText(news.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNewsList.size();
+        }
+
+    }
+
+
+}
